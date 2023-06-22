421b6dc054c88e8e83b8ad97770d14f4baab2aac
NIKHIL <sk2208d223@gmail.com>
2023-06-22 20:37:36 +0530

    SECOND COMMIT

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+
 df = {row.letter:row.code for (index, row) in data.iterrows()}
-word = input("Enter a word: ").upper()
-output_list = [df[letter] for letter in word]
-print(output_list)
+
+def generate_no():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [df[letter] for letter in word]
+    except KeyError:
+        print("sorry try with word only.")
+        generate_no()
+
+    else:
+        print(output_list)
+generate_no()
